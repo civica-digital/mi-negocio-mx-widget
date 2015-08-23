09e33dd068c8183f0736d2c3a9185d5eb050466c
mikesaurio <mikesaurio@gmail.com>
2015-08-23 00:28:56 -0500

    prueba js

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php include_once("widget-dinamico.html"); ?>
+echo "
+<script type=\"text/javascript\">
+$(document).ready(function cambioTexto(){
+	var cnt=0, texts=[];
+	$(".textContent").each(function() {
+		texts[cnt++]=$(this).text();
+	});
+	function slide() {
+		if (cnt>=texts.length) cnt=0;
+		$('#textMessage').html(texts[cnt++]);
+		$('#textMessage')
+		.fadeIn('slow').animate({opacity: 1.0}, 35000).fadeOut('slow', 
+			function() {
+				return slide()
+			}
+			);      
+	}      
+	slide();       
+});
+</script>
+";
